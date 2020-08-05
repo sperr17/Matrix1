7443255a05b36f9850048146dbaf3ff70cb53765
sperr <s.perrena@gmail.com>
2020-08-05 19:01:31 -0400

    Created my own version of a matrix

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-matrix= [
-    [1,2,3],
-    [1,2,3],
-    [1,2,4]
+import random
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
 ]
-print(matrix[-1])
+print(matrix[-2])
+
+# members=['josh','mary','mosh']
+# leader = random.choice(members)
+# print(leader)
+
+row = random.choice(matrix)
+print(row)
+
